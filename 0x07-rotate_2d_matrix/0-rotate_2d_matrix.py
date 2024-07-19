7e1290b513abe2dcce7494a3e5de1a3b4cbf7486
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-07-19 06:04:31 +0100

    rotate a 2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" Rotate 2D Matrix
-
-Manipulating rows of a matrix by reversing their order as part of the rotation process.
-Nested Loops:
-
-Using nested loops to iterate through 2D data structures like matrices.
-Modifying elements within nested loops to achieve the desired rotation.
-
+"""2D matrix rotation module.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
+    """Rotates an m by n 2D matrix in place.
     """
-    for x, y in enumerate(zip(*reversed(matrix))):
-        matrix[x] = list(y)
-
-
-if __name__ == '__main__':
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    """ rotate_2d_matrix(matrix) """
-    rotate_2d_matrix(matrix)
-    print(matrix)
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
